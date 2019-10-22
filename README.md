d119e9e4ceae56ee8ae40223753b19d35f131434
Pete Sheurpukdi <38145881+pserket@users.noreply.github.com>
2019-10-21 20:04:30 -0700

    add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# dsc106hw2
+IXD Lab7 - winter 2017
+====
+
+Lab 7 for Intro HCI: Analytics and A/B Testing
---
+++ public/js/mcd.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+});
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+$(".btn.btn-default.likeBtn").click(analytics);
+}
+
+function analytics(e) {
+	event.preventDefault();
+	ga('create', 'UA-114588818-1', 'auto');
+	ga("send", "event", 'like', 'click');
+}
